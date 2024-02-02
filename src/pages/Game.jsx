779cf211f70c7602ba56a85a6de21112213a82c4
jskartes <joshuakartes@gmail.com>
2024-02-02 15:41:03 -0700

    restructure game board layout

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,6 +3,19 @@ import Card from '../components/Card';
 const Game = () => {
   return (
     <div className='Game'>
+      <div className='game-window'>
+        <div className='left'></div>
+
+        <div className='right'>
+          <div id='opponent-in-play'></div>
+          <div id='user-in-play'></div>
+          <div id='user-hand'></div>
+        </div>
+      </div>
+
+      <div className='game-status'>
+
+      </div>
     </div>
   );
 }
