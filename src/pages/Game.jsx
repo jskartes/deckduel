2e846eeca5d2734b8fb1252b300b639cf01a03c0
jskartes <joshuakartes@gmail.com>
2024-02-03 23:47:05 -0700

    render sample cards in board state

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Card from '../components/Card';
 
 const Game = ({ game, setGame }) => {
   const [gameState, setGameState] = useState(game);
@@ -22,17 +23,21 @@ const Game = ({ game, setGame }) => {
             <span className='player-health'>
               {gameState.players[1].health}
             </span>
-            <div className='opponent-discard'></div>
+            <div className='opponent-discard'>
+              <span>Discard Pile</span>
+            </div>
           </div>
 
           <div className='user-data'>
+            <div className='user-discard'>
+              <span>Discard Pile</span>
+            </div>
             <span className='player-health'>
               {gameState.players[0].health}
             </span>
             <span className='player-name'>
               {gameState.players[0].player.username}
             </span>
-            <div className='user-discard'></div>
           </div>
         </div>
 
@@ -41,7 +46,29 @@ const Game = ({ game, setGame }) => {
           
           <div className='user-in-play'></div>
           
-          <div className='user-hand'></div>
+          <div className='user-hand'>
+            <Card
+              name='Mana'
+              art='/card-art/mana.jpeg'
+              cardType='Resource'
+              text='Increases your available mana by 1 mana'
+            />
+            <Card
+              name='Goblin Grunt'
+              cost='1'
+              art='/card-art/goblin_grunt.jpeg'
+              cardType='Monster'
+              strength='1'
+              defense='1'
+            />
+            <Card
+              name='Lightning Bolt'
+              cost='3'
+              art='/card-art/lightning_bolt.jpeg'
+              cardType='Spell'
+              text='Deals 3 damage to any target'
+            />
+          </div>
         </div>
       </div>
 
